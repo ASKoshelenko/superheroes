ba1835773348f3b58753757332cd5b5f251e7d1a
ASK <39233929+ASKoshelenko@users.noreply.github.com>
2022-10-14 13:18:51 +0300

    Add files via upload

--- client/src/pages/SuperheroPage.js
+++ client/src/pages/SuperheroPage.js
@@ -24,37 +24,35 @@ const SuperheroPage = () => {
     return (
         <Container className="mt-3">
             <Row>
-                <Col md={4}>
+                <Col md={6}>
                     <Card
                         className="d-flex flex-column align-items-center justify-content-around"
-                        style={{width: 300, height: 300, fontSize: 32, border: 'white'}}
+                        style={{width: 300, height: 200, fontSize: 32, border: 'white'}}
                     >
                         <h1><b>{superhero.name}</b></h1>
+                        <Button>Change info</Button>
 
                     </Card>
                 </Col>
-                <Col md={4}>
-                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + superhero.img}/>
+                <Col md={6}>
+                    <Image width={400} height={400} src={process.env.REACT_APP_API_URL + superhero.img}/>
                 </Col>
-                <Col md={4}>
-                    <Card
-                        className="d-flex flex-column align-items-center justify-content-around"
-                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
-                    >
-                        <h3>Real name: {superhero.real_name} </h3>
-                        {/* <Button variant={"outline-dark"} onClick={add} >Add to favourite</Button> */}
-                        {/* <Button variant={"outline-dark"} onClick={speak} >Speak</Button> */}
-
-                    </Card>
-                </Col>
+                
             </Row><br/>
             <Row className="d-flex flex-column m-3">
-                <h2><b>Description</b></h2>
-                {superhero.info.map((info, index) =>
-                    <Row key={info.id} style={{border: '2px solid lightgray', background: index % 2 === 0 ? 'seashell' : 'azure', padding: 10}}>
-                        <b>{info.title}:</b><i>{info.description}</i>
-                    </Row>
-                )}
+                <h2><b>About superhero</b></h2>
+                <Row>
+                    <h3>Real name: {superhero.real_name} </h3>
+                </Row>
+                <Row>
+                    <h4>{`Origin description:  `} {superhero.origin_description}</h4> 
+                </Row>
+                <Row>
+                    <h4>{`Catch phrase:  `} {superhero.catch_phrase}</h4>
+                </Row>
+                <Row>
+                    <h4>{`Super powers:  `} {superhero.super_powers}</h4>
+                </Row>
             </Row>
         </Container>
     );
